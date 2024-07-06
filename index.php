40b5b7712cfad1acb07ec530f4018ef80469d7c4
Cleberson Rocha <clebersonrochaads@gmail.com>
2024-07-06 19:32:35 -0300

    Chore: Retrieve data from database

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
 <?php
     include_once("templates/header.php");
 ?>
-    <h1>Test on contact list</h1>
+    <div class="container">
+        <?php if(isset($printMsg) && $printMsg != ''): ?>
+            <p id="msg"><?= $printMsg ?></p>
+        <?php endif; ?>
+        <h1 id="main-title">My list</h1>
+        <?php if(count($contacts) > 0): ?>
+            <table class="table" id="contacts-table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">#</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($contacts as $contact): ?>
+                        <tr>
+                            <td scope="row"><?= $contact["id"] ?></td>
+                            <td scope="row"><?= $contact["name"] ?></td>
+                            <td scope="row"><?= $contact["phone"] ?></td>
+                            <td class="actions">
+                                <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                                <a href="#"><i class="fas fa-edit edit-icon"></i></a>
+                                <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php else: ?>
+            <p id="empty-list-text">There are no contact in your address book list, <a href="<?= $BASE_URL ?>create.php">click here to add</a>.</p>
+        <?php endif; ?> 
+    </div>
 <?php
     include_once("templates/footer.php");
 ?>
