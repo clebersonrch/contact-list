013897266162c0cf1f52623441a813aa2b53980f
Cleberson Rocha <clebersonrochaads@gmail.com>
2024-07-06 18:52:37 -0300

    Feat: Create templates and update style

--- create.php
+++ create.php
@@ -1,7 +1,7 @@
 <?php
     include_once("templates/header.php");
 ?>
-    <h1>Test on contact list</h1>
+    <h1>Create contact</h1>
 <?php
     include_once("templates/footer.php");
 ?>
---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<?php
+    include_once("config/url.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact List</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="List">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>index.php">Contact List</a>
+                    <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>create.php">Add contact</a>
+                </div>
+            </div>
+        </nav>
+    </header>
